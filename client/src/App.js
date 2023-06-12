d2564d28e0320823ac568dd55ab9705ec03cb139
edwardahn136 <126621906+ahnedward11@users.noreply.github.com>
2023-06-11 20:33:31 -0700

    6/11
    starting to implement messaging

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import Home from "./pages/home/Home";
 import Login from "./pages/login/Login";
 import Profile from "./pages/profile/Profile";
 import Register from "./pages/register/Register";
+import Messenger from "./pages/messenger/Messenger";
 import {
   BrowserRouter as Router,
   Switch,
@@ -25,7 +26,8 @@ function App() {
         <Route path="/" element={user ? <Home/> : <Register/>} exact />
         <Route path="/login" element={user ? <Navigate to ="/" /> :<Login/>} exact />
         <Route path="/register" element={user ? <Navigate to ="/" /> :<Register/>} exact />
-        <Route path="/profile/:username" element={<Profile/>} exact />
+        <Route path="/profile/:username" element={!user ? <Navigate to="/" /> : <Messenger />} exact />
+        <Route path="/messenger" element={<Messenger/>} exact />
         {/* <Route exact path="/">
           element = {Login}
         </Route> */}
