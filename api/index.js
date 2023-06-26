8f0b08ef316fbc8c3d77e0d71ea1e26e9d49d975
ahnedward11 <126621906+ahnedward11@users.noreply.github.com>
2023-06-25 23:36:56 -0700

    build

--- api/index.js
+++ api/index.js
@@ -52,6 +52,7 @@ app.use("/api/users", userRoute);
 app.use("/api/posts", postRoute);
 app.use("/api/conversations", conversationRoute);
 app.use("/api/messages", messageRoute);
+// app.use(cors({origin}))
 
 app.listen(8800, () => {
   console.log("Backend server is running!");
